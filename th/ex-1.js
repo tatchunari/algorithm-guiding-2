746b8e87e8684a5f6aef3a2c97c3767529d5ad47
CodePawsDev <isripen1991@outlook.com>
2025-04-09 11:36:39 +0700

    feat: add answer to exercise 1

--- th/ex-1.js
+++ th/ex-1.js
@@ -6,7 +6,21 @@
 */
 
 function heaviestBagPair(bagWeights) {
-  // Start Coding Here
+  if (bagWeights.length < 2) return null; // ถ้ากระเป๋ามีไม่ถึง 2 ใบ ให้ return null
+
+  let maxWeight = 0;
+  let bestPair = [];
+
+  for (let i = 0; i < bagWeights.length - 1; i++) {
+    let currentWeight = bagWeights[i] + bagWeights[i + 1];
+
+    if (currentWeight > maxWeight) {
+      maxWeight = currentWeight;
+      bestPair = [bagWeights[i], bagWeights[i + 1]];
+    }
+  }
+
+  return bestPair;
 }
 
 console.log(heaviestBagPair([5, 8, 12, 15, 7]));
